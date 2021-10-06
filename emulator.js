1006c454bc47b7fb6a3eed05590989d1dea44b47
avshukan <avshukan@mail.ru>
2021-10-06 23:05:26 +1100

    try: scrape several pages

--- emulator.js
+++ emulator.js
@@ -3,73 +3,73 @@ const now = require('performance-now');
 const puppeteer = require('puppeteer');
 
 const emulator = async () => {
-    const result = [];
-    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1,
-    });
+  const result = [];
+  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  });
 
-    async function closeEmulator() {
-        await browser.close();
-        console.log('emulator closed');
-    }
+  async function closeEmulator() {
+    await browser.close();
+    console.log('emulator closed');
+  }
 
-    async function getPageInfo(url) {
-        console.log('============');
-        console.log(url);
-        const start = now();
-        try {
-            console.log('try...');
-            await page.goto(url, { waitUntil: 'networkidle2' });
-            const data = [];
-            let element = await page.evaluate(async () => {
-                const { default: capture } = await import( 'https://esm.sh/html2canvas' );
-                const { default: { recognize } } = await import( 'https://esm.sh/tesseract.js');
-                const rows = document.querySelectorAll('.table-responsive tr');
-                const result = [];
-                for( const row of [rows[0], rows[2], rows[22]]) {
-                    const source = row.children[2];
-                    const image = await capture( source, { imageTimeout: 1 } );
-                    // console.log( `%c `, `font-size:1px;padding: ${image.height/2}px ${image.width/2}px; background: url(${ image.toDataURL() })` );
-                    const { data: { text } } = await recognize( image );
-                    // console.log( text );
-                    const values = text.split( /\n/g ).filter( Boolean );
-                    const name = row.children[1].textContent.split('.').filter((v,i,a) => (i !== 0)  || (i === a.length-1)).join('').trim();
-                    const value = values[0];
-                    result.push([ name, value ]);
-                }
-                console.table( result );
-                return result;
-            });
-            console.log('element', element);
-            const end = now();
-            console.log('Готово! Время выполнения = ', (end - start).toFixed(3), start.toFixed(3), end.toFixed(3));
-            result.push({
-                status: 'SUCCESS',
-                payload: data,
-            });
-        } catch (error) {
-            const end = now();
-            console.log('Ошибка! Время ожидания = ', (end - start).toFixed(3), start.toFixed(3), end.toFixed(3));
-            result.push({
-                status: 'ERROR',
-                error,
-            });
+  async function getPageInfo(url) {
+    console.log('============');
+    console.log(url);
+    const start = now();
+    try {
+      console.log('try...');
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      const data = [];
+      const element = await page.evaluate(async () => {
+        const { default: capture } = await import('https://esm.sh/html2canvas');
+        const { default: { recognize } } = await import('https://esm.sh/tesseract.js');
+        const rows = document.querySelectorAll('.table-responsive tr');
+        const result = [];
+        for (const row of [rows[0], rows[2], rows[22]]) {
+          const source = row.children[2];
+          const image = await capture(source, { imageTimeout: 1 });
+          // console.log( `%c `, `font-size:1px;padding: ${image.height/2}px ${image.width/2}px; background: url(${ image.toDataURL() })` );
+          const { data: { text } } = await recognize(image);
+          // console.log( text );
+          const values = text.split(/\n/g).filter(Boolean);
+          const name = row.children[1].textContent.split('.').filter((v, i, a) => (i !== 0) || (i === a.length - 1)).join('').trim();
+          const value = values[0];
+          result.push([name, value]);
         }
-    }
-
-    async function getResult() {
+        console.table(result);
         return result;
+      });
+      console.log('element', element);
+      const end = now();
+      console.log('Готово! Время выполнения = ', (end - start).toFixed(3), start.toFixed(3), end.toFixed(3));
+      return {
+        status: 'SUCCESS',
+        payload: element,
+      };
+    } catch (error) {
+      const end = now();
+      console.log('Ошибка! Время ожидания = ', (end - start).toFixed(3), start.toFixed(3), end.toFixed(3));
+      return {
+        status: 'ERROR',
+        error,
+      };
     }
+  }
 
-    return {
-        closeEmulator,
-        getPageInfo,
-        getResult,
-    };
+  async function getResult() {
+    return result;
+  }
+
+  return {
+    closeEmulator,
+    getPageInfo,
+    getResult,
+  };
 };
 
 module.exports = emulator;
--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const DOTENV_OPTIONS = {};
 const { DOTENV_CONFIG_PATH } = process.env;
 if (DOTENV_CONFIG_PATH != null) DOTENV_OPTIONS.path = DOTENV_CONFIG_PATH;
 require('dotenv').config(DOTENV_OPTIONS);
+const fsp = require('fs/promises');
 const tiks = require('./tiks.json');
 const uiks = require('./uiks.json');
 const emulator = require('./emulator');
+
 const host = 'http://www.vybory.izbirkom.ru';
 
 (async () => {
-    const { closeEmulator, getPageInfo, getResult } = await emulator();
-    uiks
-        // .filter(({ index }) => index === 0)
-        .filter((value, index) => index === 0)
-        .forEach(async (value) => {
-            const url = `${host}/${value.tikHref}`;
-            console.log(url);
-            await getPageInfo(url);
-            const result = await getResult();
-            console.log('result', result);
-        });
-    // await closeEmulator();
+  const { closeEmulator, getPageInfo, getResult } = await emulator();
+  uiks
+    // .filter(({ index }) => index === 0)
+    .filter((value, index) => index <= 2)
+    .forEach(async (value) => {
+      const filename = `uik_${value.uikName.split('№')[1]}.json`;
+      const url = `${host}/${value.tikHref}`;
+      console.log(url);
+      const tikInfo = await getPageInfo(url);
+      const content = { ...value, ...tikInfo.payload };
+      await fsp.writeFile(filename, JSON.stringify(content), 'utf-8');
+    });
+  // await closeEmulator();
 })();
